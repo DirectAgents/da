1fd5a3ca59dcf23f448ec4fd1a3c586547d86543
Aaron Anodide <greentunes@gmail.com>
2011-07-06 13:37:04 -0400

    cosmetic refactor

--- 1/work/Tree/LendingTreeLib/LendingTreeLib/UserControlBase.cs
+++ 1/work/Tree/LendingTreeLib/LendingTreeLib/UserControlBase.cs
@@ -3,6 +3,9 @@ using System.Web.UI.WebControls;
 
 namespace LendingTreeLib
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class UserControlBase : System.Web.UI.UserControl
     {
         public PageBase PageBase
@@ -15,29 +18,34 @@ namespace LendingTreeLib
 
         /// <summary>
         /// This method is designed to be used with a TextBox web control.
-        /// It also requires that the rendered page include the CheckValue javascript function.
-        /// It causes the text box to have a default value when no data is present.
-        /// This default value is typically instructions like 'enter your name here'.
-        /// When the text box gets focus (i.e. the user is going to type now) the default message vanishes.
-        /// If the text box loses foxus and still has no data, the default value reappears.
+        /// 
+        /// The rendered page must include the CheckValue javascript function.
+        /// 
+        /// The text box will have a default value when no data is present, typically 
+        /// instructions like 'enter your name here'.
+        /// 
+        /// When the text box receives the focus the default message clears out so
+        /// s/he can type.
         /// </summary>
-        /// <param name="webControl"></param>
-        /// <param name="waterMarkText"></param>
-        protected void AddWatermark(WebControl webControl, string waterMarkText)
+        /// <param name="target"></param>
+        /// <param name="defaultText"></param>
+        protected void AddWatermark(WebControl target, string defaultText)
         {
-            var webControlClientID = webControl.ClientID;
+            var webControlClientID = target.ClientID;
             var varName = "waterMarkTextFor" + webControlClientID;
+            
             var clientScriptKey = "waterMarkScriptFor" + webControlClientID;
             var clientScriptIdentifyingType = this.GetType();
-            var clientScript = Page.ClientScript;
+            var clientScript = Page.ClientScript;    
             if (!clientScript.IsStartupScriptRegistered(clientScriptIdentifyingType, clientScriptKey))
             {
-                string script = string.Format("var {0}='{1}';", varName, waterMarkText);
+                var script = string.Format("var {0}='{1}';", varName, defaultText);
                 clientScript.RegisterStartupScript(clientScriptIdentifyingType, clientScriptKey, script, true);
             }
+
             var attrivuteValue = "CheckValue(this, " + varName + ")";
-            webControl.Attributes.Add("onfocus", attrivuteValue);
-            webControl.Attributes.Add("onblur", attrivuteValue);
+            target.Attributes.Add("onfocus", attrivuteValue);
+            target.Attributes.Add("onblur", attrivuteValue);
         }
     }
 }
