ce8e30946a011060ba851b51e73d07364c0bb1d2
Aaron Anodide <greentunes@gmail.com>
2011-07-14 16:47:09 -0400

    ltv version2

--- 1/work/Tree/LendingTreeWebSite1/Controls/Page2/Scripts/script.js
+++ 1/work/Tree/LendingTreeWebSite1/Controls/Page2/Scripts/script.js
@@ -1,13 +1,26 @@
-﻿var g = {};
+﻿//-----------------------------------------------
+// global context
+//-----------------------------------------------
+var g = {};
+
 g.MaxLTV = 0.97;
 g.Val_DDL = '#ApproximatePropertyValue1_DropDownList1';
 g.Val_Selected = false;
 g.Val = 0;
+
 g.Amt_MortBal_Selected = false;
 g.Amt_MortBal_Limited = false;
 g.Amt_MortBal = 0;
 g.Amt_MortBal_DDL = '#MortgageBalance1_DropDownList1';
 
+g.Amt_CashOut_Selected = false;
+g.Amt_CashOut_Limited = false;
+g.Amt_CashOut = 0;
+g.Amt_CashOut_DDL = '#AmountDesiredAtClosing1_DropDownList1';
+
+//-----------------------------------------------
+// drop down change event handlers
+//-----------------------------------------------
 $(document).ready(function () {
     $(g.Val_DDL).change(function () {
         log.debug('Val_DDL changed');
@@ -15,7 +28,6 @@ $(document).ready(function () {
             g.Val = parseInt($(this).val());
             log.debug('Val=' + g.Val);
             g.Val_Selected = true;
-            log.debug('Val_Selected=' + g.Val_Selected);
         }
         catch (e) {
             log.debug('exception caught');
@@ -25,44 +37,79 @@ $(document).ready(function () {
             log.debug('Val=' + g.Val);
         }
         FixAvailableMortgageBalances();
-        //FixAvailableCashOutOptions();
+        FixAvailableCashOutOptions();
+    });
+    $(g.Amt_MortBal_DDL).change(function () {
+        log.debug('Amt_MortBal_DDL changed');
+        try {
+            g.Amt_MortBal = parseInt($(this).val());
+            log.debug('Amt_MortBal=' + g.Amt_MortBal);
+            g.Amt_MortBal_Selected = true;
+        }
+        catch (e) {
+            log.debug('exception caught');
+            g.Amt_MortBal_Selected = false;
+            log.debug('Amt_MortBal_Selected=' + g.Amt_MortBal_Selected);
+            g.Val = 0;
+            log.debug('Amt_MortBal=' + g.Amt_MortBal);
+        }
+        FixAvailableCashOutOptions();
     });
 });
 
+//-----------------------------------------------
+// LTV calculators
+//-----------------------------------------------
 function MaxLTV_MortBal(amt) {
     return (amt) < (g.Val * g.MaxLTV);
 }
-
-function FixAvailableMortgageBalances() {
-    if (g.Val_Selected && g.Val > 0) {
-
-        log.debug('fixing available mortgage balances');
-
-        ResetAvailableMortgageBalances();
-
-        log.debug('limiting available mortgage balances');
-        LimitChoices(g.Amt_MortBal_DDL, MaxLTV_MortBal);
-        g.Amt_MortBal_Limited = true;
-
-    } else {
-        log.debug('NOT fixing available mortgage balances');
-    }
+function MaxLTV_CashOut(amt) {
+    return (amt + g.Amt_MortBal) < (g.Val * g.MaxLTV);
 }
 
+//-----------------------------------------------
+// mortgage rate balance adjustments
+//-----------------------------------------------
+function FixAvailableMortgageBalances() {
+    if (g.Val_Selected && g.Val > 0) {
+        ResetAvailableMortgageBalances();
+        LimitChoices(g.Amt_MortBal_DDL, MaxLTV_MortBal);
+        g.Amt_MortBal_Limited = true;
+    }
+}
 function ResetAvailableMortgageBalances() {
     if (g.Amt_MortBal_Limited) {
-        log.debug('resetting available mortgage balances');
         ResetChoices(g.Amt_MortBal_DDL, MortgageBalanceOptions);
         g.Amt_MortBal = 0;
         g.Amt_MortBal_Selected = false;
     }
-    else {
-        log.debug('NOT resetting available mortgage balances');
+}
+
+//-----------------------------------------------
+// cash out option adjustments
+//-----------------------------------------------
+function FixAvailableCashOutOptions() {
+    if (g.Val_Selected && g.Val > 0) {
+        ResetAvailableCashOutOptions();
+        LimitChoices(g.Amt_CashOut_DDL, MaxLTV_CashOut);
+        g.Amt_CashOut_Limited = true;
+
+    }
+}
+function ResetAvailableCashOutOptions() {
+    if (g.Amt_CashOut_Limited) {
+        ResetChoices(g.Amt_CashOut_DDL, AmountDesiredAtClosingOptions);
+        g.Amt_CashOut = 0;
+        g.Amt_CashOut_Selected = false;
     }
 }
 
+//-----------------------------------------------
+// drop down list helpers
+//-----------------------------------------------
 function LimitChoices(ddl, f) {
     log.debug('limiting choices for ' + ddl);
+
     var max = 0;
     $(ddl).each(function () {
         $('option', this).each(function (i) {
@@ -89,78 +136,12 @@ function LimitChoices(ddl, f) {
         log.debug('NOT removing choices');
     }
 }
-
 function ResetChoices(ddl, options) {
     log.debug('resetting choices for ' + ddl);
     $(ddl).find('option').remove();
-    log.debug('options removed');
     $.each(options, function (val, text) {
         $(ddl).append(
             $('<option></option>').val(val).html(text)
         );
     });
-    log.debug('original options restored');
 }
-
-//    $(Amt_MortBal_DDL).change(function () {
-//        try {
-//            Amt_MortBal = parseInt($(this).val());
-//            Amt_MortBal_Selected = true;
-
-//            //            if (Val > 0) {
-//            //                FixAvailableCashOutOptions();
-//            //            }
-//            //            LimitChoices('#AmountDesiredAtClosing1_DropDownList1', MaxLTV);
-//        }
-//        catch (e) {
-//            Amt_MortBal = 0;
-//            Amt_MortBal_Selected = false;
-//        }
-//    });
-
-//var CashOutSelected = false;
-//var CashOutAmount = 0;
-//var CashOutAmountDDL = '#MortgageBalance1_DropDownList1';
-
-//function FixAvailableCashOutOptions() {
-//    if (CashOutSelected) {
-//        ResetChoices('#AmountDesiredAtClosing1_DropDownList1', AmountDesiredAtClosingOptions);
-//    }
-//    LimitChoices('#AmountDesiredAtClosing1_DropDownList1', MaxLTV);
-//}
-
-//function ResetAvailableCashOutOptions() {
-//    ResetChoices('#AmountDesiredAtClosing1_DropDownList1', MortgageBalanceOptions);
-//    CashOutAmount = 0;
-//    CashOutSelected = false;
-//}
-
-//$(document).ready(function () {
-
-//    $("#MortgageBalance1_DropDownList1").change(function () {
-//        try {
-//            Amt_MortBal = parseInt($(this).val());
-//            Amt_MortBal_Selected = true;
-//            if (Val > 0) {
-//                FixAvailableCashOutOptions();
-//            }
-//            LimitChoices('#AmountDesiredAtClosing1_DropDownList1', MaxLTV);
-//        }
-//        catch (e) {
-//            Amt_MortBal = 0;
-//            Amt_MortBal_Selected = false;
-//        }
-//    });
-
-//    $("#AmountDesiredAtClosing1_DropDownList1").change(function () {
-//        try {
-//            CashOutAmount = parseInt($(this).val());
-//            CashOutSelected = true;
-//        }
-//        catch (e) {
-//            CashOutAmount = 0;
-//            CashOutSelected = false;
-//            return;
-//        }
-//    });
-//});
