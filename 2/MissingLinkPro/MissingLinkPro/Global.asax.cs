eb4bce914a32b5ec8e0a27c30a9b0436074816fa
Jonathan Wong <jonwong84@gmail.com>
2014-12-22 10:26:30 -0500

    Layout stylization and new menu bar UI addition.

--- 2/MissingLinkPro/MissingLinkPro/Global.asax.cs
+++ 2/MissingLinkPro/MissingLinkPro/Global.asax.cs
@@ -18,14 +18,14 @@ namespace IdentitySample
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
-        /**
-         * Redirects away from application if a non-HTTPS connection is detected.
-         **/
-        protected void Application_BeginRequest()
-        {
-            if (!Context.Request.IsSecureConnection)
-                Response.Redirect("http://www.directagents.com/directlink");
-        }
+        ///**
+        // * Redirects away from application if a non-HTTPS connection is detected.
+        // **/
+        //protected void Application_BeginRequest()
+        //{
+        //    if (!Context.Request.IsSecureConnection)
+        //        Response.Redirect("http://www.directagents.com/directlink");
+        //}
 
         /**
          * For testing purposes, use this method to redirect from HTTP to HTTPS.
